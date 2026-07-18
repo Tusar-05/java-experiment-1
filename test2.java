c7eda7b6ec6a235e388bb2c7ed56fc06ef61f71c
Tusar Kanta Marei <tusarkantamarei@gmail.com>
2026-07-18 13:11:11 +0530

    2nd question of asignment

---
+++ test2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class test2{
+	
+	int num,rem,sum;
+	public void cal(){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter a number");
+	num = sc.nextInt();
+	
+	while(num>0){
+		rem=num%10;
+		sum=sum+rem;
+		num=num/10;
+		
+	}
+	System.out.println("The sum of the number is"+sum);
+
+
+	}
+	
+	public static void main(String args[]){
+	test2 ob =new test2();
+	
+	ob.cal();
+	
+	
+	
+	}
+}
