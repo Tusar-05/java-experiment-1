735f4992540aedf378c066eac229f9f1c29d7b78
Tusar Kanta Marei <tusarkantamarei@gmail.com>
2026-07-18 12:46:45 +0530

    the first program

---
+++ test1.java
@@ -0,0 +1,14 @@
+
+/*write a program that accepts two string as command line
+ argument and generates the output in the required format*/
+import java.util.*;
+class test1{
+	public static void main(String args[]){
+		System.out.println("Your name is"); 
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println("My name is"+args[0]+args[1]);
+		
+	}
+
+}
