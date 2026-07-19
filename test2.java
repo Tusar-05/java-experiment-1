445fb352ba4fa3a7cc4421e0ce06416d5b9738f9
Tusar Kanta Marei <tusarkantamarei@gmail.com>
2026-07-19 13:00:25 +0530

    Add comments to improve code clarity
    Added comments to clarify the purpose of the code and its logic.

--- test2.java
+++ test2.java
@@ -4,18 +4,19 @@ import java.util.*;
 public class test2{
 	
 	int num,rem,sum;
+	//to calculate the sum of the number entered
 	public void cal(){
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter a number");
 	num = sc.nextInt();
-	
+	//use of while loop for condition check 
 	while(num>0){
 		rem=num%10;
 		sum=sum+rem;
 		num=num/10;
 		
 	}
-	System.out.println("The sum of the number is"+sum);
+	System.out.println("The sum of the number is"+sum);//printing of the sum 
 
 
 	}
